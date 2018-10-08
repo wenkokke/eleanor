ef8e5e3b60f87b7ac2fdf37a626230c1806ae3a0
Wen Kokke <wen.kokke@gmail.com>
2018-10-08 10:27:50 -0500

    more benchmarks

--- run_benchmarks.sh
+++ run_benchmarks.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+./benchmark.sh -o benchmarks/bench.Linear.Neat.log -l 0 -u 30 -- --system Linear --strategy Neat
+
 for i in `seq 26 30`
 do
+    ./benchmark.sh -o benchmarks/bench.Untyped.Neat.log     -l $i -u $i -- --system Untyped --strategy Neat
     ./benchmark.sh -o benchmarks/bench.SimplyTyped.Feat.log -l $i -u $i -- --system SimplyTyped --strategy Feat
     ./benchmark.sh -o benchmarks/bench.Linear.Feat.log      -l $i -u $i -- --system Linear      --strategy Feat
 done
